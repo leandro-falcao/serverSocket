3b2f3c93226be2d017099ca80c28630bc1b8a3d6
leandro-falcao <leandrofalcaoti@gmail.com>
2021-05-20 18:15:07 -0300

    complete arq
    Signed-off-by: leandro-falcao <leandrofalcaoti@gmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const { log } = require('console')
 const express = require('express')
 const app = express()
 
 const http = require('http').Server(app)
-const serverSocket = require('socket.io')
+const serverSocket = require('socket.io')(http)
 
-// alterações
+const port = 3111
+
+http.listen(port, function () {
+  console.log(`servidor iniciado na porta: ${port}`);
+})
+
+app.get('/', function (req, res){
+  res.send('<h1> ola mundo</h1>')
+})
